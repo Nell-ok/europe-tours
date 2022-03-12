30a94d8f9cb02f6016681fc5e7ec2842ddb89a37
Nell-ok <nelli-nn@yandex.ru>
2022-03-12 19:33:31 +0300

    Добавление слайдов

--- source/js/script.js
+++ source/js/script.js
@@ -7,6 +7,9 @@ const buttonClose = document.querySelector(".button-close");
 const buttonCloseTooltip = document.querySelector(".button-close--tooltip");
 const popupCard = document.querySelector(".popup");
 const tooltipCard = document.querySelector(".tooltip");
+const cardLinks = document.querySelectorAll(".card__link")
+const sliderTabs = document.querySelectorAll(".slider__link");
+const descriptionCards = document.querySelectorAll(".description-item");
 
 navMain.classList.remove("main-nav--nojs");
 navMain.classList.add("main-nav--closed");
@@ -48,4 +51,36 @@ buttonCloseTooltip.addEventListener("click", () => {
   if (tooltipCard.classList.contains("tooltip--show")) {
     tooltipCard.classList.remove("tooltip--show");
   }
+});
+
+descriptionCards.forEach((card) => {
+  card.classList.add("description-item--hidden");
+});
+
+sliderTabs[0].classList.add("slider__link--current");
+descriptionCards[0].classList.remove("description-item--hidden");
+
+const showCards = (index) => {
+  sliderTabs.forEach((tab) => {
+    tab.classList.remove("slider__link--current");
+  });
+  descriptionCards.forEach((card) => {
+    card.classList.add("description-item--hidden");
+  });
+
+  sliderTabs[index].classList.add("slider__link--current");
+  descriptionCards[index].classList.remove("description-item--hidden");
+};
+
+sliderTabs.forEach((button, index) => {
+  button.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    showCards(index);
+  });
+});
+
+cardLinks.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    showCards(index);
+  });
 });
